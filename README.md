401ba93df51fbb5e54288fe8fd428d29bbd0d3cb
Thomas <hirtzt@gmail.com>
2020-10-26 16:44:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+<!-- This is commented out. -->
 # noisy-networks
-Minimal implementation of the paper "Noisy Networks for Exploration" using Pytorch.
+
+This repository provide a minimal implementation of the [Noisy Networks for Exploration](https://arxiv.org/pdf/1706.10295.pdf) using Pytorch
+
+The actual implementation of this network is avalaible on my github repository [reinforcement-learning](https://github.com/thomashirtz/reinforcement-learning)
+
+
+The noisy layers are similar to linear layer, but a noise that can be tuned with the "sigma" parameters is added.
+
+<img src="https://render.githubusercontent.com/render/math?math=\Large y=w x%2Bb">
+
+become:  
+<img src="https://render.githubusercontent.com/render/math?math=\Large y=\left(\mu^{w}%2B\sigma^{w} \odot \varepsilon^{w}\right) x %2B \mu^{b}%2B\sigma^{b} \odot \varepsilon^{b}">
---
+++ noisynetworks.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class IndependentNoisyLayer(nn.Module):
+    def __init__(self, input_features, output_features, sigma: float = 0.017):
+        super().__init__()
+
+        self.input_features = input_features
+        self.output_features = output_features
+
+        self.sigma = sigma
+        self.bound = (3/input_features)**0.5
+
+        self.mu_bias = nn.Parameter(torch.FloatTensor(output_features))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(output_features))
+        self.mu_weight = nn.Parameter(torch.FloatTensor(output_features, input_features))
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(output_features, input_features))
+
+        self.epsilon_bias = None
+        self.epsilon_weight = None
+        self.register_buffer('epsilon_bias', torch.FloatTensor(output_features))
+        self.register_buffer('epsilon_weight', torch.FloatTensor(output_features, input_features))
+
+        self.parameter_initialization()
+        self.sample_noise()
+
+    def parameter_initialization(self):
+        self.sigma_bias.data.fill_(self.sigma)
+        self.sigma_weight.data.fill_(self.sigma)
+        self.mu_bias.data.uniform_(-self.bound, self.bound)
+        self.mu_weight.data.uniform_(-self.bound, self.bound)
+
+    def forward(self, x: torch.Tensor, sample_noise: bool = True) -> torch.Tensor:
+        if not self.training:
+            return F.linear(x, weight=self.mu_weight, bias=self.mu_bias)
+
+        if sample_noise:
+            self.sample_noise()
+
+        bias = self.sigma_bias * self.epsilon_bias + self.mu_bias
+        weight = self.sigma_weight * self.epsilon_weight + self.mu_weight
+        return F.linear(x, weight=weight, bias=bias)
+
+    def sample_noise(self):
+        self.epsilon_bias = self.get_noise_tensor((self.output_features,))
+        self.epsilon_weight = self.get_noise_tensor((self.output_features, self.input_features))
+
+    def get_noise_tensor(self, features: tuple) -> torch.Tensor:
+        noise = torch.FloatTensor(*features).uniform_(-self.bound, self.bound)
+        return torch.sign(noise) * torch.sqrt(torch.abs(noise))
+
+
+class FactorisedNoisyLayer(nn.Module):
+    def __init__(self, input_features, output_features, sigma=0.5):
+        super(FactorisedNoisyLayer).__init__()
+
+        self.input_features = input_features
+        self.output_features = output_features
+
+        self.sigma = sigma
+        self.bound = input_features**(-0.5)
+
+        self.mu_bias = nn.Parameter(torch.FloatTensor(output_features))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(output_features))
+        self.mu_weight = nn.Parameter(torch.FloatTensor(output_features, input_features))
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(output_features, input_features))
+
+        self.epsilon_input = None
+        self.epsilon_output = None
+        self.register_buffer('epsilon_input', torch.FloatTensor(input_features))
+        self.register_buffer('epsilon_output', torch.FloatTensor(output_features))
+
+        self.parameter_initialization()
+        self.sample_noise()
+
+    def parameter_initialization(self):
+        self.mu_bias.data.uniform_(-self.bound, self.bound)
+        self.sigma_bias.data.fill_(self.sigma * self.bound)
+        self.mu_weight.data.uniform_(-self.bound, self.bound)
+        self.sigma_weight.data.fill_(self.sigma * self.bound)
+
+    def forward(self, x: torch.Tensor, sample_noise: bool = True) -> torch.Tensor:
+        if not self.training:
+            return F.linear(x, weight=self.mu_weight, bias=self.mu_bias)
+
+        if sample_noise:
+            self.sample_noise()
+
+        weight = self.sigma_weight * torch.ger(self.epsilon_output, self.epsilon_input) + self.mu_weight
+        bias = self.sigma_bias * self.epsilon_output + self.mu_bias
+        return F.linear(x, weight=weight, bias=bias)
+
+    def sample_noise(self):
+        self.epsilon_input = self.get_noise_tensor(self.input_features)
+        self.epsilon_output = self.get_noise_tensor(self.output_features)
+
+    def get_noise_tensor(self, features: int) -> torch.Tensor:
+        noise = torch.FloatTensor(features).uniform_(-self.bound, self.bound)
+        return torch.sign(noise) * torch.sqrt(torch.abs(noise))
