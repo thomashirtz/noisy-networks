7c7664f2b22a1bcaba51987ced13d141b81ad334
Thomas <hirtzt@gmail.com>
2020-11-11 19:16:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-setup(name='noisynetworks',
-      version='0.0.1',
-      install_requires=['torch']
+setup(
+    name='noisynetworks',
+    version='0.1.0',
+    py_modules=['noisynetworks'],
+    install_requires=['torch'],
+    entry_points='''
+        [console_scripts]
+        noisynetworks=noisynetworks:noisynetworks
+    '''
 )
